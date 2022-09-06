0a8190fcfcc03416d9ad0c55d41d69dc724ba0d0
mvn-hieupham2-dn <hieu.pham2@monstar-lab.com>
2022-09-06 14:18:05 +0700

    add list score page

--- src/pages/Topic/ListScore.js
+++ src/pages/Topic/ListScore.js
@@ -1,47 +1,87 @@
 import React, { useContext, useEffect, useState } from 'react'
 import 'antd/dist/antd.min.css'
-import { Col, Row } from 'antd'
-import TopicHeader from '../../components/Topic/TopicHeader/TopicHeader'
-import TopicBody from '../../components/Topic/TopicBody/TopicBody'
+import { Table } from 'antd'
 import useHttpClient from '../../hooks/useHttpClient'
 import { AuthContext } from '../../context/auth'
 import { useParams } from 'react-router-dom'
 import { fetchDataApi } from '../../utils/fetchDataApi'
-import './Topic.css'
+import './ListScore.css'
 
-function Quiz() {
+function ListScore() {
   const { setError } = useHttpClient()
-  const [data, setData] = useState()
+  const [data, setData] = useState([])
+  const [topic, setTopic] = useState({})
+  const [sum, setSum] = useState([])
+  const [users, setUsers] = useState([])
   const { currentUser } = useContext(AuthContext)
   const { id } = useParams()
-
-  useEffect(() => {
-    const fetchTopics = async () => {
-      try {
-        const response = await fetchDataApi(
-          `topics/${id}/scores`,
-          currentUser.accessToken,
-          'GET',
-        )
-        response && setData(response.data)
-      } catch (error) {
-        setError(error.message)
-      }
-    }
-    currentUser && fetchTopics()
+  const [loading, setloading] = useState(true)
+  useEffect(async () => {
+    currentUser && fetchScore() && setloading(false)
   }, [currentUser, id])
+  const fetchScore = async () => {
+    try {
+      const response = await fetchDataApi(
+        `topics/${id}/scores`,
+        currentUser.accessToken,
+        'GET',
+      )
+      setData(response.data)
+      if (response) {
+        console.log('response', response)
+        for (let i = 0; i < response.length; i++) {
+          console.log('hello')
+          const res = await fetchDataApi(
+            `users/${data[i].userId}`,
+            currentUser.accessToken,
+            'GET',
+          )
+          setUsers((prev) => [...prev, res.data])
+        }
+      }
+    } catch (error) {
+      console.log(error)
+      setError(error.message)
+    }
+  }
+  // const fetchDataUser = async () => {
+  //   try {
+  //     if (data) {
+
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
+  console.log('data user', users)
+  const columns = [
+    {
+      title: 'Topic',
+      dataIndex: `topicId`,
+      width: 150,
+    },
+    {
+      title: 'Name',
+      dataIndex: 'userId',
+      width: 150,
+    },
+    {
+      title: 'Score',
+      dataIndex: 'score',
+      width: 150,
+    },
+  ]
 
   return (
-    <>
-      <Row>
-        <Col xs={0} sm={3} xl={5}></Col>
-        <Col xs={24} sm={18} xl={14}>
-          {data && <></>}
-        </Col>
-        <Col xs={0} sm={3} xl={5}></Col>
-      </Row>
-    </>
+    <div>
+      <Table
+        columns={columns}
+        loading={loading}
+        dataSource={data}
+        // pagination={{ pageSize: 50 }}
+      />
+      ,
+    </div>
   )
 }
 
-export default Quiz
+export default ListScore
